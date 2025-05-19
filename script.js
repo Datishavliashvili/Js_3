2da8ec267596465d5cfa6d7f8274abbfe5165c69
Datishavliashvili <davit.shavliashvili04@gmail.com>
2025-05-19 22:06:18 +0400

    oioi

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-let age = 10;
+// // let myname = "dati";
 
-console.log("hello");
+// // let greeting = `hello`;
+// // let greeting2 = "hello";
+
+// // console.log(`${greeting} `);
+
+// // let fulltext = "asdasdasdasdasdad";
+
+// // let textlength = fulltext.length;
+
+// // console.log(fulltext[textlength]);
+
+// // console.log(fulltext.toUpperCase);
+
+// let username = prompt("დაწერეთ თქვენი სახელი");
+
+// if (username && username.includes(" ")) {
+//   console.log("თქვენი სახელი შეიცავს სფეის");
+// } else {
+//   console.log(`hello ${username}`);
+// }
